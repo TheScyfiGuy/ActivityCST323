cce9333d24ea3d2a41db26f8293a3af83c25c2e4
minit <tbrown416@my.gcu.edu>
2023-11-30 13:25:13 -0700

    Update 2

--- CST323Activity/src/main/java/com/gcu/Cst323ActivityApplication.java
+++ CST323Activity/src/main/java/com/gcu/Cst323ActivityApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Cst323ActivityApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from my Spring Boot application");
 		SpringApplication.run(Cst323ActivityApplication.class, args);
 	}
 
--- CST323Activity/src/main/java/com/gcu/controller/HomeController.java
+++ CST323Activity/src/main/java/com/gcu/controller/HomeController.java
@@ -3,23 +3,23 @@ package com.gcu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/index") // Changed from "/home"
     public String home(Model model) {
         // You can add any model attributes needed for rendering the home page
         model.addAttribute("title", "Home Page");
-        return "home"; // Assuming you have a Thymeleaf template named "home.html"
+        return "home"; // Points to home.html
     }
 
     @GetMapping("/about")
     public String about(Model model) {
         // You can add any model attributes needed for rendering the about page
         model.addAttribute("title", "About Page");
-        return "about"; // Assuming you have a Thymeleaf template named "about.html"
+        return "about"; // Points to about.html
     }
-
-    
 }
