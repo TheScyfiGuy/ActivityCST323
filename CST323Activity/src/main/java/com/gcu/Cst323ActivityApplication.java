625ce291a5721b620e02d60893b8ee5292a8452d
minit <tbrown416@my.gcu.edu>
2023-12-07 15:57:39 -0700

    Azure push

--- CST323Activity/src/main/java/com/gcu/Cst323ActivityApplication.java
+++ CST323Activity/src/main/java/com/gcu/Cst323ActivityApplication.java
@@ -2,10 +2,11 @@ package com.gcu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
 @SpringBootApplication
-
+@EntityScan("com.gcu")
 public class Cst323ActivityApplication {
 
 	public static void main(String[] args) {
